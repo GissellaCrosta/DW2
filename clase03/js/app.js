d9e03a9f07386e175d540e5270b178d0fbcad923
GissellaCrosta <48534249+GissellaCrosta@users.noreply.github.com>
2020-03-25 22:58:51 -0400

    Add files via upload

---
+++ clase03/js/app.js
@@ -0,0 +1,15 @@
+
+function escribirTablaMultiplicar(j){
+
+	console.log("Tabla de multiplicar del " + j);
+
+	//i++ significa i=i+1;
+	for(i = 1; i<=10; i++){
+		console.log(j + "x" + i + "= " + j * i);
+  }
+}
+
+escribirTablaMultiplicar(5);
+  console.log("========================================");
+escribirTablaMultiplicar(6);
+  console.log("========================================");
